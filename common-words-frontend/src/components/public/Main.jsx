e94b7a5a95124e162c9483396f0159844c83ed43
Jbo <jasonrepoforlaunchcode@gmail.com>
2025-08-05 09:10:41 -0500

    added a few lines on the main page

--- common-words-frontend/src/components/public/Main.jsx
+++ common-words-frontend/src/components/public/Main.jsx
@@ -4,10 +4,15 @@ const Main = () => {
     return (
         <>
             <div>
+                <div>
+                    Welcome to common words.
+
+                    On the study page, you can add new cards to the existing decks, or create your own.
+                </div>
                 <Link to="/Study">
                     <button type="button">Study</button>
                 </Link>
-                </div>
+            </div>
         </>
     );
 }
